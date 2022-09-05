259368319afd3f2bb35deb08fceb593bf4fb4c65
Matheus Lima <matheus.lima@ccc.ufcg.edu.br>
2022-09-04 23:17:25 -0300

    onGoing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
+import { Dashboard } from "./components/Dashboard";
+import Modal from "react-modal";
+import { Header } from "./components/Header";
+import { GlobalStyle } from "./styles/global";
+import { useState } from "react";
+import { NewTransactionModal } from "./components/NewTransactionModal";
+
+Modal.setAppElement("#root");
+
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionOpen(false);
+  }
   return (
-    <div className="App">
-      <h1>Oi</h1>
-    </div>
+    <>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+      <Dashboard />
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+      <GlobalStyle />
+    </>
   );
 }
